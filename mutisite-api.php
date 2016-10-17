4d39933f74433fb19d64b296c0041813e5aece46
Tommaso Armstrong <tomm@tomma.so>
2016-10-17 15:32:04 +1100

    API to get posts working

--- mutisite-api.php
+++ mutisite-api.php
@@ -1,29 +1,65 @@
 <?php
 /**
- * @package  multisite-api
+ * @package  clatoolkit-wp
  * @version 0.1
  */
 /*
-Plugin Name: Multisite API
+Plugin Name: CLA Toolkit WP
 Plugin URI: https://github.com/uts-cic/wp-multisite-api
-Description: Exposes an endpoint to read content from a multisite WordPress network
+Description: Plugin to connect WordPress with the CLA Toolkit
 Author: Tommaso Armstrong
 Version: 0.1
 Author URI: https://tomma.so/
 */
 
 add_action( 'rest_api_init', function () {
-    register_rest_route( 'multisite-api/v1', 'sites', array(
-        'methods' => 'GET',
-        'callback' => 'get_all_sites',
-    ) );
+	register_rest_route( 'clatoolkit-wp/v1', 'posts', array(
+		'methods' => 'GET',
+		'callback' => 'get_recent_posts',
+		'permission_callback' => function () {
+			return current_user_can( 'edit_others_posts' );
+		}
+	) );
 } );
 
-// Get all blogs
+const POSTS_PER_PAGE = 10;
 
-function get_all_sites(WP_REST_Request $request) {
-    $sites = get_sites();
+function get_recent_posts(WP_REST_Request $request) {
 
-    return $sites;
+	// Get all blogs in the network
+	$sites = get_sites();
+
+	$posts = [];
+
+	// If the page query exists, use it, otherwise default $page to 0
+	$page = isset($request['page']) ? $request['page'] : 0;
+	$offset = POSTS_PER_PAGE * $page;
+
+	// For each blog in the network
+	foreach( $sites as $site ){
+		// Switch to the blog
+		switch_to_blog( $site->blog_id );
+
+		// Get posts
+		$blog_posts = get_posts(array("posts_per_page" => POSTS_PER_PAGE, "offset" => $offset));
+
+		// Get post comments
+		for ($i = 0; $i < count($blog_posts); $i++) {
+			$comments = get_comments(array("post_id" => $blog_posts[$i]->ID));
+
+			$blog_posts[$i]->comments = $comments;
+		}
+
+		// Add the blog's posts to the posts object to return (if there are any)
+		if (count($blog_posts) > 0) {
+			$posts[] = ["blog_id" => $site->blog_id, "posts" => $blog_posts];
+		}
+
+		// Switch back to the original blog
+		restore_current_blog();
+	}
+
+	// Return all the posts to the user
+	return $posts;
+
 }
-
